b2eaa7251eb3a0beec7154b9a75d6f84d94dbf6e
Alban PRATS <alban.prats@bde-insa-lyon.fr>
2017-07-02 20:19:37 +0200

    Cannot migrate app. TODO  : python manage.py migrate python manage.py createsuperuser python manage.py runserver

--- horizon_login/settings.py
+++ horizon_login/settings.py
@@ -126,8 +126,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+OAUTH2_PROVIDER = {
+    # this is the list of available scopes
+    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
+}
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
+
+
